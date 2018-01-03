89531f85d13e990ed9bde7d07429c3ba6c6a403c
Frank <Frank@hotmail.com>
2018-01-03 16:19:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/a20180103_03/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.student.a20180103_03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    WebView wv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wv=findViewById(R.id.webview);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.setWebChromeClient(new WebChromeClient());
+        wv.loadUrl("file:///android_asset/index.html");
+    }
+}
